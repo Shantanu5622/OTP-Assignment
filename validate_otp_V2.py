12e666ef90239cf1fa8771de96b3ec586578e30b
Shantanu5622 <137330251+Shantanu5622@users.noreply.github.com>
2023-12-06 11:54:12 +0530

    Convert into Association relation UML

--- validate_otp_V2.py
+++ validate_otp_V2.py
@@ -3,57 +3,63 @@ import smtplib
 from twilio.rest import Client
 import keys  # assuming keys.py contains necessary API keys and constants
 
+class EmailSender:
+    def sendEmail(self, to_email, message):
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(keys.email_username, keys.email_password)
+        server.sendmail(keys.sender_email, to_email, message)
+        server.quit()
+
+class SMSSender:
+    def sendSMS(self, to_mobile, message):
+        client = Client(keys.account_sid, keys.auth_token)
+        msg = client.messages.create(
+            body=message,
+            from_=keys.twilio_number,
+            to=to_mobile
+        )
+
 class OTPSender:
-    def __init__(self, email_username, email_password):
-        self.email_username = email_username
-        self.email_password = email_password
+    def __init__(self, email_sender, sms_sender):
+        self.email_sender = email_sender
+        self.sms_sender = sms_sender
 
-    def generate_otp(self):
-        return ''.join([str(random.randint(0, 9)) for _ in range(6)])
+    def generateOTP(self):
+        otp = ''.join([str(random.randint(0, 9)) for i in range(6)])
+        return otp
 
-    def send_otp_over_email(self, email, otp):
-        if self.validate_email(email):
-            server = smtplib.SMTP('smtp.gmail.com', 587)
-            server.starttls()
-            server.login(self.email_username, self.email_password)
-            message = self._compose_email_message(otp)
-            server.sendmail(self.email_username, email, message)
+    def sendOTPOverEmail(self, email, otp):
+        if self.validateEmail(email):
+            msg = f'Hello, your OTP is {otp}\nPlease do not share the OTP with anyone.\nThis is a system-generated email, so do not reply.'
+            self.email_sender.sendEmail(email, msg)
             print(f"OTP is sent to {email} via email.")
-            server.quit()
         else:
             print("Invalid email address.")
 
-    def send_otp_over_mobile(self, mobile, otp):
-        if self.validate_mobile(mobile):
-            client = Client(keys.account_sid, keys.auth_token)
-            message = self._compose_mobile_message(otp)
-            client.messages.create(body=message, from_=keys.twilio_number, to=mobile)
+    def sendOTPOverMobile(self, mobile, otp):
+        if self.validateMobile(mobile):
+            msg = f'Hello, your OTP is {otp}\nPlease do not share the OTP with anyone.\nThis is a system-generated message, so do not reply.'
+            self.sms_sender.sendSMS(mobile, msg)
             print(f"OTP is sent to {mobile} via mobile.")
         else:
             print("Invalid mobile number.")
 
-    def validate_email(self, email):
+    def validateEmail(self, email):
         return "@gmail" in email and "." in email
 
-    def validate_mobile(self, mobile):
+    def validateMobile(self, mobile):
         return len(mobile) == 10 and mobile.isdigit()
 
-    def _compose_email_message(self, otp):
-        return f'Hello, your OTP is {otp}\nPlease do not share the OTP with anyone.\nThis is a system-generated email, so do not reply.'
-
-    def _compose_mobile_message(self, otp):
-        return f'Hello, your OTP is {otp}\nPlease do not share the OTP with anyone.\nThis is a system-generated message, so do not reply.'
-
 if __name__ == "__main__":
-    email_username = 'shantanumetkari331@gmail.com'
-    email_password = 'szvsrrdpvpmgwnbf'
-
-    otp_sender = OTPSender(email_username, email_password)
+    email_sender = EmailSender()
+    sms_sender = SMSSender()
+    otp_sender = OTPSender(email_sender, sms_sender)
 
     email = "metkarishantanu441@gmail.com"
-    otp = otp_sender.generate_otp()
-    otp_sender.send_otp_over_email(email, otp)
+    otp = otp_sender.generateOTP()
+    otp_sender.sendOTPOverEmail(email, otp)
 
     mobile = "9604750155"
-    otp = otp_sender.generate_otp()
-    otp_sender.send_otp_over_mobile(mobile, otp)
+    otp = otp_sender.generateOTP()
+    otp_sender.sendOTPOverMobile(mobile, otp)
